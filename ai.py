c1a43088475962a206418f4088135e16f12d23d5
kamiltarach <kamiltarach.kt@gmail.com>
2023-06-02 21:44:50 +0200

    pogoda
    Jeszcze nie dokonca dziala

--- ai.py
+++ ai.py
@@ -35,5 +35,25 @@ if("otwórz" in text and word_list[0] == "otwórz") or ("uruchom" in text and wo
         webbrowser.open_new_tab("https://www.google.com/")
 
 elif text =="Jaka jest pogoda":
-    subprocess.call("explorer.exe shell:appsFolder\\Microsoft.BingWeather_8wekyb3d8bbwe!App", shell=True, 
-                    stdout=subprocess.PIPE)
+    
+    api_key = "e2b5320f9a8e8ce7371dca2624000eae"
+
+    base_url = "http://api.openweathermap.org/data/2.5/weather?q="
+    city = "warsaw"
+    url = base_url + city + "&appid=" + api_key
+    response = requests.get(url)
+    x = response.json
+    y = x["main"]
+
+    temp = y['temp']
+    odczuwalna = y['feels_like']
+    cisnienie = y['pressure']
+    wilgotnosc = y['humidity']
+
+    today = date.today().strftime("%d/%m/%Y")
+    toaster = ToastNotifier()
+    toaster.show_toast("Pogoda na dziś ("+ today +")",
+            "Temperatura: "+ str(round(temp - 273.15))+ "C°"+
+            "\nOdczuwalna: "+ str(round(odczuwalna - 273.15))+ "C°"+
+            "\nCiśnienie: "+str(cisnienie)+"hPa"+
+            "\nWilgotność: "+str(wilgotnosc)+"%", duration=None)
